e0c46ba8c296eff4f8ee6e822122ed8eb7b2018f
Luis Cruz Mac <luismario2712@gmail.com>
2023-07-12 20:45:17 -0600

    se agregan controllers

--- src/Routes/alumnos_routes.js
+++ src/Routes/alumnos_routes.js
@@ -1,41 +1,11 @@
 const express = require('express')
-const alumnosJson = require('../Models/alumnos.json')
+const router = express.Router()
+const alumnoController = require('../Controllers/alumnoController');
 
-const route = express.Router()
+// Estas son las rutas de mi API
+router.get('/obtenerAlumnos', alumnoController.obtenerAlumnos);
+router.post('/agregarAlumno', alumnoController.agregarAlumno);
+router.patch('/actualizarAlumno', alumnoController.actualizarAlumno);
+router.delete('/borrarAlumno', alumnoController.borrarAlumno);
 
-//Aquí debo ponder todo mi CRUD de alumnos
-
-route.get('/obtenerAlumnos', (req, res) => { //Endpoint
-    res.json(alumnosJson)
-})
-
-route.post('/agregarAlumno', (req, res) => {
-    alumnosJson.alumnos.push(req.body)
-    res.json(alumnosJson)
-})
-
-route.patch('/actualizarAlumno', (req, res) => {
-    const index = alumnosJson.alumnos.findIndex(alumno => alumno.boleta === req.body.boleta)
-    console.log(index);
-
-    if (index >= 0) {
-        alumnosJson.alumnos[index] = req.body
-        return res.json(alumnosJson)
-    } else {
-        return res.json({message:"No existe alumno"})
-    }
-})
-
-route.delete('/eliminarAlumno', (req, res) => {
-    const index = alumnosJson.alumnos.findIndex(alumno => alumno.boleta === req.body.boleta)
-    console.log(index);
-
-    if (index >= 0) {
-        alumnosJson.alumnos.splice(index, 1)
-        return res.json(alumnosJson)
-    } else {
-        return res.json({message:"No existe alumno"})
-    }
-})
-
-module.exports = route
+module.exports = router;
